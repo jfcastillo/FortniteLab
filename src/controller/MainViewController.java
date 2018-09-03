d901b96b07603cffc692cd8738510a65f648d6ce
jfcastillo <jfcastillo1204@gmail.com>
2018-09-03 00:04:37 -0500

    JavaFX project created

---
+++ src/controller/MainViewController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class MainViewController implements Initializable{
+
+	public MainViewController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
