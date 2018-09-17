dd61dca5ff57e1990667bc7ad13776eeb6a54d64
Sergio <sergio_andres_29@hotmail.com>
2018-09-16 20:59:07 -0500

    The queue interface was added

---
+++ src/collections/IQueue.java
@@ -0,0 +1,12 @@
+package collections;
+
+public interface IQueue<E> {
+	
+	public boolean isEmpty();
+	public void insertElement(E element);
+	public boolean isFull();
+	public void remove();
+	
+	
+
+}
