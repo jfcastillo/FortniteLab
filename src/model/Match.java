26909123553c7cfcc08f8adb73127c7ef684a179
Sergio <sergio_andres_29@hotmail.com>
2018-09-16 20:10:17 -0500

    Added a method that is the poisson probability that is used to know the probability that a number of players find a game in a certain time.

--- src/model/Match.java
+++ src/model/Match.java
@@ -11,7 +11,28 @@ public class Match {
 	}
 	
 	
-	 
+	 public int factorial(int num) {
+			int factorial=1;
+			
+			for (int i = num; i > 0; i--) {
+	            factorial = factorial * i;
+	        }
+			
+			
+			return factorial;
+			
+		}
+		
+		
+		public double probablyPoisson(int k) {
+			double probably=0;
+			double  u=0;
+			probably=(Math.pow(u, k)*Math.pow(Math.E,-u))/factorial(k);
+			
+			
+			
+			return probably;
+		}
 	
 
 }
